8e92c2a15a071076e9403ba5a06aacf6dfac0243
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-20 15:17:16 +0800

    finishing the rest of personal info

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ from tkinter import ttk
 # main window
 main = Tk()
 main.title("COVID Contact Tracing App")
-main.geometry("500x200")
+main.geometry("400x175")
 
 # create a frame inside a window
 widget = ttk.Frame(main)
@@ -16,11 +16,20 @@ widget.pack()
 
 # personal information
 user_info = ttk.Labelframe(widget, text="Personal Information")
-user_info.grid(row=0, column=0)
+user_info.grid(row=0, column=0, padx=20, pady=10)
 
 # first name
 first_name_label = ttk.Label(user_info, text="First Name:")
-first_name_label.grid(row=0, column=0)
+first_name_label.grid(row=0, column=0, padx=10, pady=(5, 0), sticky=EW)
+
+first_name_entry = ttk.Entry(user_info)
+first_name_entry.grid(row=1, column=0, padx=10, pady=(0, 5), sticky=EW)
+
+last_name_label = ttk.Label(user_info, text="Last Name:")
+last_name_label.grid(row=2,column=0, padx=10, sticky=EW)
+
+last_name_entry = ttk.Entry(user_info)
+last_name_entry.grid(row=3,column=0, padx=10, pady=(0, 5), sticky=EW)
 
 # COVID details
 # add entry
