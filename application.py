e6819237a1d9f078b6bd20bcdd058f8a3a51fcf2
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-20 15:47:32 +0800

    covid details; vax status

--- application.py
+++ application.py
@@ -8,7 +8,6 @@ from tkinter import ttk
 # main window
 main = Tk()
 main.title("COVID Contact Tracing App")
-main.geometry("400x175")
 
 # create a frame inside a window
 widget = ttk.Frame(main)
@@ -25,6 +24,7 @@ first_name_label.grid(row=0, column=0, padx=10, pady=(5, 0), sticky=EW)
 first_name_entry = ttk.Entry(user_info)
 first_name_entry.grid(row=1, column=0, padx=10, pady=(0, 5), sticky=EW)
 
+# last name
 last_name_label = ttk.Label(user_info, text="Last Name:")
 last_name_label.grid(row=2,column=0, padx=10, sticky=EW)
 
@@ -32,6 +32,19 @@ last_name_entry = ttk.Entry(user_info)
 last_name_entry.grid(row=3,column=0, padx=10, pady=(0, 5), sticky=EW)
 
 # COVID details
+covid_details = ttk.Labelframe(widget, text="COVID Details")
+covid_details.grid(row=1, column=0, padx=20, pady=10)
+
+# vaccination status
+vax_status_list = ["Not Vaccinated", "Vaccinated - 1st Dose", "Vaccinated - 2nd Dose", "Vaccinated - 1st Booster", "Vaccinated - 2nd Booster"]
+
+vax_label = ttk.Label(covid_details, text="Vaccination Status:")
+vax_label.grid(row=0, column=0, padx=10, pady=(5, 0), sticky=EW)
+
+vax_combobox = ttk.Combobox(covid_details, values=vax_status_list)
+vax_combobox.current(2)
+vax_combobox.grid(row=6, column=0, padx=10, pady=(0, 5), sticky=EW)
+
 # add entry
 
 # create a frame for data
