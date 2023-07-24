c49c801ce234c1f2c1c9232f9219cce98e105875
Katrina Pabuna <kinapabuna@gmail.com>
2023-07-24 10:28:27 +0800

    final output (revisions beforehand)

--- application_ui.py
+++ application_ui.py
@@ -14,6 +14,14 @@ class Application(Tk):
         self.title("COVID Contact Tracing App")
         self.configure(bg="#B2BEB5")
     
+    # def clear_btn function
+        def clear_btn_func():
+            first_name_entry.delete(0, END)
+            last_name_entry.delete(0, END)
+            self.vax_var.set("(Select Vaccination Status)")
+            self.exp_var.set("")
+            self.test_var.set("")
+    
     # def add_entry function
         def add_entry():
             # open the csv file and write data
@@ -24,7 +32,7 @@ class Application(Tk):
                 write.writerow([first_name_entry.get().capitalize(), last_name_entry.get().capitalize(),
                                 self.vax_var.get(), self.exp_var.get(), self.test_var.get()])
 
-            # clearing the 
+            # clearing the form after submitting an entry
             first_name_entry.delete(0, END)
             last_name_entry.delete(0, END)
             self.vax_var.set("(Select Vaccination Status)")
@@ -41,6 +49,7 @@ class Application(Tk):
 
             # def search_entry function
             def search_entry_func():
+                # opening the csv file
                 with open("details.csv") as file:
                     name = search_entry.get()
                     data = []
@@ -49,29 +58,36 @@ class Application(Tk):
                     for row in csv_reader:
                         data.append(row)
 
+                    # only for the last name
                     row_set = []
-                    row_set = [row[1].lower() for row in data]
+                    row_set = [row[1].lower() for row in data] # converts the items in the list to lowercase
 
+                    # finding the searched input in the list of last names
                     for row in range(0, len(data)):
                         for item in row_set:
-                            if name.lower() == data[row][1].lower(): # allows the user to type in lowercase
+                            if name.lower() == data[row][1].lower(): # converts the user's input to lowercase
 
                                 # creating a frame to display the searched entry
                                 disp_frame = ttk.Frame(search_entry_window)
                                 disp_frame.grid(row=1, column=0, padx=20, pady=(0, 15))
 
+                                # display the first name
                                 disp_fname = ttk.Label(disp_frame, text="First Name: " + str(data[row][0]))
                                 disp_fname.grid(row=0, column=0, padx=20)
 
+                                # display the last name
                                 disp_lname = ttk.Label(disp_frame, text="Last Name: " + str(data[row][1]))
                                 disp_lname.grid(row=1, column=0, padx=20)
 
+                                # display the vax status
                                 disp_vax = ttk.Label(disp_frame, text="Vaccination Status: " + str(data[row][2]))
                                 disp_vax.grid(row=2, column=0, padx=20)
-                                        
+                                
+                                # display the answer to exposure
                                 disp_exp = ttk.Label(disp_frame, text="Exposure to COVID: " + str(data[row][3]))
                                 disp_exp.grid(row=3, column=0, padx=20)
 
+                                # display the answer to COVID test
                                 disp_test = ttk.Label(disp_frame, text="Tested for COVID: " + str(data[row][4]))
                                 disp_test.grid(row=4, column=0, padx=20)
 
@@ -111,25 +127,25 @@ class Application(Tk):
 
     # create a frame inside a window
         widget = ttk.Frame(self)
-        widget.pack(padx=20, pady=15)
+        widget.grid(row=0, column=0, padx=(20, 0), pady=15)
 
     # personal information
         user_info = ttk.Labelframe(widget, text="Personal Information")
-        user_info.grid(row=0, column=0, padx=20, pady=10, sticky=EW)
+        user_info.grid(row=0, column=0, padx=(20), pady=10)
 
         # first name
         first_name_label = ttk.Label(user_info, text="First Name:")
-        first_name_label.grid(row=0, column=0, padx=10, pady=5)
+        first_name_label.grid(row=0, column=0, padx=(20, 10), pady=5)
 
         first_name_entry = ttk.Entry(user_info)
-        first_name_entry.grid(row=0, column=1, padx=(5, 10), pady=(0, 5))
+        first_name_entry.grid(row=0, column=1, padx=(0, 20), pady=(0, 5))
 
         # last name
         last_name_label = ttk.Label(user_info, text="Last Name:")
-        last_name_label.grid(row=1,column=0, padx=10, pady=(0, 10))
+        last_name_label.grid(row=1,column=0, padx=(20, 10), pady=(0, 10))
 
         last_name_entry = ttk.Entry(user_info)
-        last_name_entry.grid(row=1,column=1, padx=(5, 10), pady=(0, 10))
+        last_name_entry.grid(row=1,column=1, padx=(0, 20), pady=(0, 10))
 
     # COVID details
         covid_details = ttk.Labelframe(widget, text="COVID Details")
@@ -141,11 +157,11 @@ class Application(Tk):
         self.vax_var = StringVar(self)
 
         vax_label = ttk.Label(covid_details, text="Please select your vaccination status:")
-        vax_label.grid(row=0, column=0, padx=10, pady=(5, 0), sticky=EW)
+        vax_label.grid(row=0, column=0, padx=20, pady=(5, 0), sticky=EW)
 
         vax_opt = ttk.OptionMenu(covid_details, self.vax_var, *vax_status_list)
         vax_opt.configure(direction="below")
-        vax_opt.grid(row=1, column=0, padx=10, pady=(0, 10), sticky=EW)
+        vax_opt.grid(row=1, column=0, padx=20, pady=(0, 10), sticky=EW)
 
         # exposure to COVID
         self.exp_var = StringVar(self)
@@ -180,10 +196,22 @@ class Application(Tk):
         test_rdbtn4 = ttk.Radiobutton(covid_details, text="Yes - Pending", variable=self.test_var, value="Yes - Pending")
         test_rdbtn4.grid(row=10, column=0, padx=10, pady=(0, 5), sticky=EW)
 
-    # add entry
-        submit_btn = ttk.Button(widget, text="Submit", command=add_entry)
-        submit_btn.grid(row=2, column=0, padx=20, pady=(0, 10))
+    # another widget to hold the 4 commands
+        widget2 = ttk.Frame(self)
+        widget2.grid(row=0, column=1,padx=(0, 20), pady=15)
 
-    # search entry
-        search_btn = ttk.Button(widget, text="Search", command=search_entry_window)
-        search_btn.grid(row=3, column=0, padx=20, pady=(0, 10))
+        # clear button
+        clear_btn = ttk.Button(widget2, text="Clear", command=clear_btn_func)
+        clear_btn.grid(row=0, column=1, padx=(0, 20), pady=(143, 10))
+
+        # add entry
+        submit_btn = ttk.Button(widget2, text="Submit", command=add_entry)
+        submit_btn.grid(row=1, column=1, padx=(0, 20), pady=(0, 10))
+
+        # search entry
+        search_btn = ttk.Button(widget2, text="Search", command=search_entry_window)
+        search_btn.grid(row=2, column=1, padx=(0, 20), pady=(0, 10))
+
+        # exit button
+        exit_btn = ttk.Button(widget2, text="Exit", command=exit)
+        exit_btn.grid(row=3, column=1, padx=(0, 20), pady=(0, 143))
